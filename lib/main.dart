bec58759d941137c7c0b0bd7da80442633f785e3
Kei Fujikawa <fujikei22funk@gmail.com>
2023-08-14 20:53:43 +0900

    シルバースポンサーにフリーベル株式会社さん追加

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class AppTheme {
           primary: AppColor.primaryNavyColor,
           secondary: AppColor.primaryYellowColor,
         ),
-    scaffoldBackgroundColor: AppColor.themeRed,
+    scaffoldBackgroundColor: AppColor.backgroundBlack,
     cardTheme: const CardTheme(
       margin: EdgeInsets.zero,
     ),
